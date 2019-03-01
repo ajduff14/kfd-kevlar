abf71332b665f0dba8a15ebe1683847e8edfa916
Adam <adam@kingfisherdirect.co.uk>
2019-03-01 16:32:41 +0000

    Add steps to remove default aws-quickstart setup

--- scripts/dev/configure_magento.sh
+++ scripts/dev/configure_magento.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $# -ne 14 ]; then
-    echo $0: usage: install_magento.sh dbhost dbuser dbpassword dbname cname adminfirstname adminlastname adminemail adminuser adminpassword magentourl protocol magentolanguage magentocurrency magentotimezone
+    echo $0: usage: install_magento.sh dbhost dbuser dbpassword dbname cname adminfirstname adminlastname adminemail adminuser adminpassword magentourl magentolanguage magentocurrency magentotimezone
     exit 1
 fi
 
@@ -18,10 +18,9 @@ adminemail=$8
 adminuser=$9
 adminpassword=${10}
 magentourl=${11}
-protocol=${12}
-magentolanguage=${13}
-magentocurrency=${14}
-magentotimezone=${15}
+magentolanguage=${12}
+magentocurrency=${13}
+magentotimezone=${14}
 
 cd
 #curl -o magento.tar.gz $magentourl
@@ -31,10 +30,12 @@ cd
 #    echo "Error downloading media from s3"
 #    exit 1
 #fi
-[ -f "magento.tar.gz" ] && echo "Media file Found" || echo "Media file Not found"
+
+[ -f $app_archive ] && echo "Media file Found" || echo "Media file Not found"
+ 
+sudo tar xzfP ~/magento.tar.gz --directory /var/www/html
 
 cd /var/www/html
-tar xzfP ~/magento.tar.gz
 
 find var vendor pub/static pub/media app/etc -type f -exec chmod g+w {} \;
 find var vendor pub/static pub/media app/etc -type d -exec chmod g+ws {} \;
@@ -45,19 +46,23 @@ cat << EOF > /var/www/html/pub/health
 OK
 EOF
 
-if [ "$protocol" = "http" ]
-then
-  secure="--use-secure-admin=0 --use-secure=0"
-else
-  secure="--use-secure-admin=1 --use-secure=1 --base-url-secure=$protocol://$cname/"
-fi
 
 cd /var/www/html/bin
-./magento setup:install --base-url=$protocol://$cname/ \
---db-host=$dbhost --db-name=$dbname --db-user=$dbuser --db-password=$dbpassword \
---admin-firstname=$adminfirst --admin-lastname=$adminlast --admin-email=$adminemail \
---admin-user=$adminuser --admin-password=$adminpassword --language=$magentolanguage \
---currency=$magentocurrency --timezone=$magentotimezone $secure
+./magento setup:install \
+  --base-url=https://$cname/ \
+  --db-host=$dbhost \
+  --db-name=$dbname \
+  --db-user=$dbuser \
+  --db-password=$dbpassword \
+  --admin-firstname=$adminfirst \
+  --admin-lastname=$adminlast \
+  --admin-email=$adminemail \
+  --admin-user=$adminuser \
+  --admin-password=$adminpassword \
+  --language=$magentolanguage \
+  --currency=$magentocurrency \
+  --timezone=$magentotimezone \
+  --use-secure-admin=1 --use-secure=1 --base-url-secure=https://$cname/
 
 init_value=`head -n10 /var/www/html/app/etc/env.php`
 
@@ -168,15 +173,6 @@ EOF
 mysql -h $dbhost -u $dbuser $dbname -p$dbpassword << EOF
 INSERT INTO core_config_data (scope, scope_id, path, value) VALUES ('default', 0, 'dev/static/sign', 1)
 EOF
-
-if [ "$protocol" = "http" ]
-then
-
-mysql -h $dbhost -u $dbuser $dbname -p$dbpassword << EOF
-INSERT INTO core_config_data (scope, scope_id, path, value) VALUES ('default', 0, 'web/url/redirect_to_base', 0)
-EOF
-
-fi
 
 ./magento deploy:mode:set production
 
